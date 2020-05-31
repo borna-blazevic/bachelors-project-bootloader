cb1e71ec96bdd308e019e75d313f338b4a1a25ec
borna-blazevic <borna.blazevic@fer.hr>
2020-05-31 19:52:17 +0200

    complete firmware upgrade protocol

--- include/usart.h
+++ include/usart.h
@@ -7,7 +7,7 @@
 #include <stm32f4xx_gpio.h>  // gpio control
 #include <stm32f4xx_usart.h> // USART
 
-#define BUFSIZE 16
+#define BUFSIZE 256
 #define BAUDRATE 115200
 
 void USART1_Init(void);       // init USART1 peripheral
--- main.c
+++ main.c
@@ -8,48 +8,102 @@ int main(void)
 {
 	NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x00000);
 	prvSetupHardware();
-	gpio_led_state(LED3_ORANGE_ID, 1);
-	uint32_t *bootenv = (uint32_t *) &_shared;
+	uint32_t *bootenv = (uint32_t *)&_shared;
 
-	if (*bootenv != 1 && *bootenv != 2 && *bootenv != 3)
+	if (bootenv[3] != 1 && bootenv[3] != 2)
 	{
-		*bootenv = 1;
+		bootenv[3] = 1;
 		print_string("Initial message\n");
-	}
-	if (*bootenv == 3)
-	{
-		print_string("booting updated\n");
-		void **_app_start= (void **)&_new_image;
-		((void (*)())_app_start[1])();
-	}
-	else if (*bootenv == 1 )
-	{
 		void **_app_start = (void **)&_recovery_image;
 		((void (*)())_app_start[1])();
 	}
-	else if (*bootenv == 2 )
+	else if (bootenv[3] == 1)
 	{
-		if(bootenv[55]){
-			uint32_t *firmware_beggining =  (uint32_t *)&_firmware_upgrade;
-			uint32_t *firmware_end = (uint32_t *)bootenv[50];
+
+		if (bootenv[4] == 1)
+		{
+			uint32_t *firmware_beggining = (uint32_t *)&_firmware_upgrade;
+			uint32_t *firmware_end = (uint32_t *)bootenv[5];
 			uint32_t *flash_start = &_new_image;
-			uint32_t *flash_end = flash_start + (firmware_end-firmware_beggining);
+			uint32_t *flash_end = flash_start + (firmware_end - firmware_beggining);
 
 			unlock_flash();
-			if(erase_flash_blocks((uint32_t)flash_start, (uint32_t)flash_end)){
+			if (erase_flash_blocks((uint32_t)flash_start, (uint32_t)flash_end))
+			{
 				print_string("failed flash at erase\n");
 				*bootenv = 1;
 			}
-			if(write_flash_and_check_blocks((uint32_t)flash_start, (uint32_t)flash_end, firmware_beggining)){
+			if (write_flash_and_check_blocks((uint32_t)flash_start, (uint32_t)flash_end, firmware_beggining))
+			{
 				print_string("failed flash at write\n");
 				*bootenv = 1;
 			}
 			print_string("flashed and checked\n");
-			bootenv[55] = 0;
+			bootenv[4] = 0;
 			lock_flash();
+			print_string("booting updated\n");
+			void **_app_start = (void **)&_new_image;
+			((void (*)())_app_start[1])();
 		}
-		void **_app_start = (void **)&_recovery_image;
-		((void (*)())_app_start[1])();
+		else
+		{
+			print_string("booting recovery\n");
+			void **_app_start = (void **)&_recovery_image;
+			((void (*)())_app_start[1])();
+		}
+	}
+	else if (bootenv[3] == 2)
+	{
+		if (bootenv[4] == 1)
+		{
+			uint32_t *firmware_beggining = (uint32_t *)&_firmware_upgrade;
+			uint32_t *firmware_end = (uint32_t *)bootenv[5];
+			uint32_t *flash_start = &_new_image;
+			uint32_t *flash_end = flash_start + (firmware_end - firmware_beggining);
+
+			unlock_flash();
+			if (erase_flash_blocks((uint32_t)flash_start, (uint32_t)flash_end))
+			{
+				print_string("failed flash at erase\n");
+				bootenv[3] = 1;
+				print_string("booting recovery\n");
+				void **_app_start = (void **)&_recovery_image;
+				((void (*)())_app_start[1])();
+			}
+			if (write_flash_and_check_blocks((uint32_t)flash_start, (uint32_t)flash_end, firmware_beggining))
+			{
+				print_string("failed flash at write\n");
+				bootenv[3] = 1;
+				print_string("booting recovery\n");
+				void **_app_start = (void **)&_recovery_image;
+				((void (*)())_app_start[1])();
+			}
+			print_string("flashed and checked\n");
+			bootenv[4] = 0;
+			lock_flash();
+			print_string("booting updated\n");
+			bootenv[3] = 1;
+			void **_app_start = (void **)&_new_image;
+			((void (*)())_app_start[1])();
+		}
+		else
+		{
+			for (int i = 0; i < bootenv[0]; i++)
+			{
+				if ((bootenv[1] >> i) && (bootenv[2] >> i))
+				{
+					print_string("mistake happened\n");
+					bootenv[3] = 1;
+					print_string("booting recovery\n");
+					void **_app_start = (void **)&_recovery_image;
+					((void (*)())_app_start[1])();
+				}
+				print_string("booting updated\n");
+				bootenv[3] = 1;
+				void **_app_start = (void **)&_new_image;
+				((void (*)())_app_start[1])();
+			}
+		}
 	}
 }
 
